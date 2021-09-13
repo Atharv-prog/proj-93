14788fa17e48b53efdbfb73927d0be9463337833
Atharv-prog <72858850+Atharv-prog@users.noreply.github.com>
2021-09-13 14:21:44 +0530

    Add files via upload

---
+++ screens/IssLocation.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,  } from 'react-native';
+
+export default class IssLocationScreen extends Component {
+    render(){
+        return(
+         <View style={{flex:1, justifycontent:"center",alignItems:"center"}}>
+          <Text>ISS Location Screen</Text>
+         </View>
+        
+         )
+        }
+
+} 
---
+++ screens/Meteors.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,  } from 'react-native';
+
+export default class MeteorScreen extends Component{
+render(){
+ return(
+  <View style={{flex:1, justifycontent:"center",alignItems:"center"}}>
+   <Text>Meteor info Screen</Text>
+  </View>
+ 
+  )
+ }
+}
